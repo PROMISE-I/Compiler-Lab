dd8f8e636a434661e886709703a0bbba9630b4a0
tracer-compiler2022 <compilers@software.nju.edu.cb>
2022-11-16 10:41:21 +0800

    >  make 201250185 root Linux LAPTOP-SGA0NGID 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:41:21 up  2:07,  0 users,  load average: 0.00, 0.00, 0.00 690e94933eee47be4eebf5fe5ed079b6a4695da0

--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -11,7 +11,7 @@ public class MyErrorListener extends BaseErrorListener {
 
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        System.err.println("Error type A at line " + line + ":" + msg);
+        System.err.print("Error type A at line " + line + ":" + msg);
         hasError = true;
     }
 }
